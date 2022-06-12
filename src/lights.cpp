d574994e4e4683c9554152142d118dacac279d05
Kevin Clement <kevin.clement@gmail.com>
2022-06-12 01:41:20 -0700

    cleanup sample code

--- src/lights.cpp
+++ src/lights.cpp
@@ -9,97 +9,10 @@
 #define HUE_GREEN       96  // Green: found in example lib, under HUE_GREEN  
 #define MAX_LOOPS       5
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
-int loops = 0;
-uint8_t pos = NUM_LEDS;
-bool sweepRight = true;
-
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-CRGBPalette16 currentPalette = RainbowColors_p;
-TBlendType currentBlending = LINEARBLEND;
-#define UPDATES_PER_SECOND 100
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-    uint8_t brightness = 255;
-
-      colorIndex = 0;
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
-}
-
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-}
-
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
-  }
-}
-
-void rainbowWithGlitter() 
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
-}
-
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
-}
-
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  uint8_t dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
-
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE(gPatterns);
-}
+CRGB leds[NUM_LEDS];      // the array of leds
+int loops = 0;            // how many loops we've done on a solve
+uint8_t pos = NUM_LEDS;   // tracks position during a sweep
+bool sweepRight = true;   // should we sweep right or left
 
 Lights::Lights(Logic &logic)
 : _logic(logic)
@@ -211,7 +124,8 @@ void Lights::fadeInAndOut() {
 void Lights::fancyPants() {
 
   EVERY_N_MILLISECONDS(120) {
-     gHue++;
+    static uint8_t gHue = 0;
+    gHue++;
 
     // random colored speckles that blink in and fade smoothly
     fadeToBlackBy(leds, NUM_LEDS, 3);
