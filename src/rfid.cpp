65a1d31a541ad72ca6037e8ae29a2902a92525db
Kevin Clement <kevin.clement@gmail.com>
2022-06-12 23:11:22 -0700

    add known valid RFID tags

--- src/rfid.cpp
+++ src/rfid.cpp
@@ -4,19 +4,27 @@
 
 #define NUM_VALID_TAGS 2
 byte VALID_TAGS [2][NUM_VALID_TAGS][4] = {
+
+  // F2|WHITE tags
   {
-    { 0xF7, 0x6F, 0x8C, 0xF2 },
-    { 0x87, 0x93, 0x8E, 0xF2 }
+    { 0x04, 0x16, 0x22, 0x0A }, // KING
+    { 0x04, 0xBF, 0x21, 0x0A }  // QUEEN
   },
+
+  // C7|BLACK tags
   {
-    { 0x27, 0x8F, 0x8E, 0xF2 },
-    { 0xE3, 0xB8, 0xE0, 0x91 }
+    { 0x04, 0xC7, 0x21, 0x0A },  // KING 
+    { 0x04, 0x97, 0x1F, 0x0A }   // QUEEN
   }
 };
 
 Rfid::Rfid(Logic &logic)
 : _logic(logic),
+
+  // F2
   nfc1(logic, A3, 21, "NFC1", VALID_TAGS[0], NUM_VALID_TAGS),
+
+  // C7
   nfc2(logic, A4, A5, "NFC2", VALID_TAGS[1], NUM_VALID_TAGS)
 {
 }
