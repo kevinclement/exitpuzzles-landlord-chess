1596a179387b7d96d4c703b2bb9e92ab6c8a8c28
Kevin Clement <kevin.clement@gmail.com>
2022-12-09 22:46:03 -0800

    once bust is solved leave it solved

--- src/bust.cpp
+++ src/bust.cpp
@@ -18,11 +18,20 @@ void Bust::handle() {
   bool _prevSwitched = isSwitched;
   int btnState = digitalRead(BTN_SWITCH_PIN);
 
-  if (!btnState) {
-      pressedTime = 0;
-      isSwitched = false;
-  }
-  else {
+  // if (!btnState) {
+  //     pressedTime = 0;
+  //     isSwitched = false;
+  // }
+  // else {
+  //     if (pressedTime == 0) {
+  //         pressedTime = millis();
+  //     }
+  //     else if (millis() - pressedTime > BOUNCE_TIME_MS) {
+  //         isSwitched = true;
+  //     }
+  // }
+
+  if (btnState && !isSwitched) {
       if (pressedTime == 0) {
           pressedTime = millis();
       }
