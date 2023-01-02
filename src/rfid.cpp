dd4f49a26c21a8a54ad13080a6e54b73501612d1
Kevin Clement <kevin.clement@gmail.com>
2023-01-02 02:09:50 -0800

    add label to rfid class for easy lookup

--- src/rfid.cpp
+++ src/rfid.cpp
@@ -5,10 +5,10 @@
 Rfid::Rfid(Logic &logic)
 : _logic(logic),
 
-  // F2
+  // F2 (BLACK-QUEEN)
   nfc1(logic, A3, 21, "F2", F2),
 
-  // C7
+  // C7 (WHITE-KING)
   nfc2(logic, A4, A5, "C7", C7)
 {
 }
